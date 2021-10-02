234a4934aded95ffc19136bdf84c120170a79468
George Bennett <george6bennett@gmail.com>
2021-10-02 16:09:25 +0100

    add nomiclabs verification

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +19,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+   networks: {
+     ropsten: {
+      url: "https://polygon-rpc.com",
+      accounts: [process.env.LOOT_BRIDGE_PRIVATE_KEY],
+    }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGON_SCAN_API_KEY
+  }
 };
