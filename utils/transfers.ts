e7034f760830da3432dbb5fa9dd693904b986cb7
George Bennett <george6bennett@gmail.com>
2021-10-04 20:06:21 +0100

    move infra to top level

--- utils/transfers.ts
+++ utils/transfers.ts
@@ -3,9 +3,6 @@ import { Contract, ethers } from "ethers";
 export const recentLootTransfers = (
   ethProvider: ethers.providers.JsonRpcProvider
 ) => {
-  /*
-   * Fetch recent Loot transfers
-   */
   const lootContract = new Contract(
     "0xff9c1b15b16263c61d017ee9f65c50e4ae0113d7",
     [
