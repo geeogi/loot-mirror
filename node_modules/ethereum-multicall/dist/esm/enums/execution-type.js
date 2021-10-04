6e5388f32b3f5be86024756c661e8074d73ad374
George Bennett <george6bennett@gmail.com>
2021-10-04 20:38:06 +0100

    remove node_modules

--- node_modules/ethereum-multicall/dist/esm/enums/execution-type.js
+++
@@ -1,6 +0,0 @@
-export var ExecutionType;
-(function (ExecutionType) {
-    ExecutionType["web3"] = "web3";
-    ExecutionType["ethers"] = "ethers";
-    ExecutionType["customHttp"] = "custom";
-})(ExecutionType || (ExecutionType = {}));
--- node_modules/ethereum-multicall/dist/esm/index.js
+++
@@ -1,2 +0,0 @@
-export * from './models/multicall-options';
-export { Multicall } from './multicall';
